8e575f4f634bd5757fc93d4793d253eb4b9fad07
András Benjamin Bezzeg <xedbab@INF.ELTE.HU>
2019-11-19 10:48:25 +0100

    018 - Camera: Input with GLFW

--- Window.h
+++ Window.h
@@ -10,15 +10,21 @@ class Window
 public:
 	Window();
 	Window(GLint windowWidth, GLint windowHeight);
-
+	// general
 	int Initialize();
 
 	GLfloat getBufferWidth() { return bufferWidth; };
 	GLfloat getBufferHeight() { return bufferHeight; };
 
+	void swapBuffers() { glfwSwapBuffers(mainWindow); };
+
 	bool getShouldClose() { return glfwWindowShouldClose(mainWindow); };
 
-	void swapBuffers() { glfwSwapBuffers(mainWindow); };
+	// camera 
+	bool* getKeys() { return keys; };
+	GLfloat getXChange();
+	GLfloat getYChange();
+
 
 	~Window();
 	
@@ -27,6 +33,22 @@ private:
 
 	GLint width, height;
 	GLint bufferWidth, bufferHeight;
+
+	// general
+	void createCallBacks();
+	bool keys[1024];
+
+	// mouse movement
+	GLfloat lastX;
+	GLfloat lastY;
+	GLfloat xChange;
+	GLfloat yChange;
+	static void handleMouse(GLFWwindow* window, double xPos, double yPos);
+
+	// keyboard 
+	bool mouseFirstMoved;
+	static void handleKeys(GLFWwindow* window, int key , int code , int action , int mode);
+	
 
 };
 
